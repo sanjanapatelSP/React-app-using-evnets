a0bbf0b9b1e1fa3c455daacdbbaf8cd2203797e6
sanjanapatel <iampatel.sanjana@gmail.com>
2024-01-06 04:28:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+const App =()=>{
+   let text="Hello Guys !!"
+   const [tcolor, setTcolor]=useState(text);
+   
+// this function is used for changing text
+  const textChange =()=>{
 
-function App() {
+   text ="Welcome in React World 💯!"
+     setTcolor(text)
+  }
+
+  const [bg,setBg]=useState("#8e44ad")
+   const [name,setName]=useState("Click me 😄") 
+
+// this function is used for changing backgroundColor and button text
+
+   const bgBack=()=>{
+    setBg("black");
+    setName("ouch!! 😧")
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={ { backgroundColor:bg}}>
+      <h1>{tcolor}</h1>
+      <button onDoubleClick={bgBack}  onMouseLeave={textChange}>{name}     </button>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
